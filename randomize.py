62bc736f561bf4719187d665bd06aab96f1315f0
ScorpiusX <113597322+ScorpiusX@users.noreply.github.com>
2023-11-03 21:02:39 +0100

    new input

---
+++ randomize.py
@@ -0,0 +1,11 @@
+my_list = [1, 2, 3, 4, 5]
+
+# Initialize a variable to count the elements
+count = 0
+
+# Iterate through each element in the list
+for _ in my_list:
+    count += 1  # Increment the count for each element
+
+# Print the total count
+print("The number of elements in the list is:", count)
