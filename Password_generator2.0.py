5956db72ba6707a548d3a97ed67ec2236beb2ce1
ScorpiusX <113597322+ScorpiusX@users.noreply.github.com>
2023-11-13 15:36:36 +0100

    few changes

--- Password_generator2.0.py
+++ Password_generator2.0.py
@@ -9,19 +9,22 @@ nr_letters= int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
+# random.choice()
+# random.shuffle()
+
 password_list = []
 
-for char in range(1, nr_letters +1):
+for char in range(1, nr_letters + 1):
     password_list += random.choice(letters)
-
 for char in range(1, nr_symbols + 1):
     password_list += random.choice(symbols)
-
 for char in range(1, nr_numbers + 1):
     password_list += random.choice(numbers)
 
 print(password_list)
+
 random.shuffle(password_list)
+
 print(password_list)
 
 password = ""
@@ -30,5 +33,3 @@ for char in password_list:
     password += char
 
 print(password)
-
-
