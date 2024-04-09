9653116933aac1097a2584e56a1c5f207d8922f6
Scorpius-X <eric.bolade@gmail.com>
2024-04-09 13:09:32 +0100

    finished

--- Ticket_calculator.py
+++ Ticket_calculator.py
@@ -3,23 +3,31 @@ age = int(input("Please input your age: "))
 
 if height > 120:
     print("Yes you can ride")
+    ticket = 0
+    if age >= 18:
+        ticket += 12
+    elif age >= 12 and age <= 18:
+        ticket += 7
+    else:
+        ticket += 5
+    photo = input("Do you want photos?, Please input yes or no: ").lower()
+    if photo == "yes":
+        ticket += 3
+        print(f"Your total bill is {ticket}$")
+    else:
+        print(f"Your total bill is {ticket}$")
+
+
+
+
 else:
     print("No sorry you can't ride")
 
-ticket = 0
 
-if age >= 18:
-    ticket += 12
-elif age >= 12 and age <= 18:
-    ticket += 7
-else:
-    ticket += 5
 
-photo = input("Do you want photos?, Please input yes or no: ").lower()
 
-if photo == "yes":
-    ticket += 3
-    print(f"Your total bill is {ticket}$")
-else:
-    print(f"Your total bill is {ticket}$")
+
+
+
+
 
