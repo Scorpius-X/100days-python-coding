05c67ee985878486ef4888566833c7ae5b7ffd00
Scorpius-X <eric.bolade@gmail.com>
2024-04-09 18:19:32 +0100

    finished

--- Ticket_calculator.py
+++ Ticket_calculator.py
@@ -6,13 +6,17 @@ if height > 120:
     ticket = 0
     if age >= 18:
         ticket += 12
+        print("Adult tickets cost $12")
     elif age >= 12 and age <= 18:
         ticket += 7
+        print("Youth tickets cost $7")
     else:
         ticket += 5
+        print("children tickets cost $5")
     photo = input("Do you want photos?, Please input yes or no: ").lower()
     if photo == "yes":
         ticket += 3
+        print("photos cost $3")
         print(f"Your total bill is {ticket}$")
     else:
         print(f"Your total bill is {ticket}$")
