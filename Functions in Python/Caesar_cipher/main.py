9dc7086d698de916c138d1bc6f867d8f1549514f
Scorpius-X <eric.bolade@gmail.com>
2024-05-19 15:59:02 +0100

    modified a bit

--- Functions in Python/Caesar_cipher/main.py
+++ Functions in Python/Caesar_cipher/main.py
@@ -1,11 +1,11 @@
 # create a caesar cipher code that encodes and decodes messages, accepting inputs as well as asking whether to end or continue
 from art import logo
 from Caesar_cipher import alphabet
-
 def caesar(start_text, shift_number, cypher_direction):
     cipher_text = ""
     if cypher_direction == "decode":
         shift_number*= -1
+
     for char in start_text:
         if char in alphabet:
             position = alphabet.index(char)
@@ -13,18 +13,20 @@ def caesar(start_text, shift_number, cypher_direction):
             cipher_text += new_position
         else:
             cipher_text += char
+
+
     print(f"Your {cypher_direction}d text is {cipher_text}")
 
-print(logo)
 should_continue = True
-
+print(logo)
 while should_continue:
-    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-    text = input("Type your message:\n").lower()
-    shift = int(input("Type the shift number:\n"))
+    direction = input("Please choose whether to 'encode' or 'decode' your message: \n").lower()
+    text = input("Please type in your message: ").lower()
+    shift = int(input("Please input the shift number: "))
     shift = shift % 26
     caesar(start_text= text, shift_number= shift, cypher_direction= direction)
-    result = input("Type 'yes' if you want to go again. Otherwise type 'no': ").lower()
+    result = input("Type 'yes' or 'no' if you would like to continue: ")
+
     if result == "no":
         should_continue = False
-        print("Goodbye")
+        print("Thanks Goodbye")
