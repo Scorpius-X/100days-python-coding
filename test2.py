99ff52b0cff8cf21ec6bf3fc136b403019676bef
Scorpius-X <eric.bolade@gmail.com>
2024-09-09 07:57:05 +0100

    revising

---
+++ test2.py
@@ -0,0 +1,7 @@
+def my_function(f_name, l_name):
+    print(f"Hello!, {f_name} {l_name}")
+
+f_name = input("Please input your firstname: ").title()
+l_name = input("Please input your lastname: ").title()
+
+my_function(f_name, l_name)
