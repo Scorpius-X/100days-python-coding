371f3a2eea54c30d72656d62b1b7a790f719172e
Eric Bolade <eric.bolade@gmail.com>
2025-02-10 08:49:11 +0100

    done

---
+++ Day 17 start/main.py
@@ -0,0 +1,11 @@
+#creating a social media platform
+
+class User:
+
+    def __init__(self, user_id, username):
+        self.id = user_id
+        self.username = username
+        self.followers = 0
+        self.following = 0
+
+    def follow(self):
