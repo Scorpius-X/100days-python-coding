3604a3ccd788134609e8632ef7fcac03add586e6
Eric Bolade <eric.bolade@gmail.com>
2025-02-23 06:52:20 +0100

    ok

---
+++ Guessing game/Functions in Python/greet.py
@@ -0,0 +1,26 @@
+# def greet():
+#     print("Good morning folks")
+#     print("How are you doing today?")
+#     print("Do have a wonderful day")
+
+# greet()
+
+# def greet_with_name(name):
+#     print(f"Hello, Good morning {name}")
+#     print(f"How do you do today {name}")
+#     print(f"Quite a wonderful weather we are having tonight right {name}?")
+
+# greet_with_name("Eric")
+# greet_with_name("Daniel")
+
+# def greet_with(name, location):
+#     print(f"Hello {name}")
+#     print(f"What is it like in {location}")
+
+# greet_with("Eric", "Lagos")
+
+def greet_anyone(a = " ", b = " ", c = " "):
+    print(f"hello {a}")
+    print(f"what is it like in {b}")
+
+greet_anyone("lagos", "Eric")
